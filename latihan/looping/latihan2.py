e7070fea4930b8102d2fc848878219696e4658fa
azrilrizkiansyah <azrilrizkiansyah11@gmail.com>
2024-11-20 10:07:02 +0700

    menambahkan program

---
+++ latihan/looping/latihan2.py
@@ -0,0 +1,11 @@
+#program cetak teks sebanyak 100 kali dengan looping 
+#inisiasi variabel perlurangan
+x = 1 
+
+#perulangan sebanyak 100 kali 
+while x <= 100 :
+    print("Saya anak sekolah SMK Bina Kerja")
+    print("Kelas x-ppl")
+    #tambahkan x dengan 1 setiap perulangan
+    x+=1
+    
